fb5f0a5b920657469430265272cb512cb8d05c55
Garbox0 <gbrlescalada@gmail.com>
2026-01-29 14:21:29 -0300

    fix(analytics): actually render GoogleAnalytics component in body

--- app/layout.js
+++ app/layout.js
@@ -39,6 +39,7 @@ export default async function RootLayout({ children }) {
     <html lang="es" suppressHydrationWarning>
       <body className={`${outfit.variable} ${playfair.variable}`} suppressHydrationWarning>
         {children}
+        <GoogleAnalytics gaId="G-YGHD9G2S0R" />
       </body>
     </html>
   );
