3bc69b47e9669ff051725ae8d4ef0b27268046d6
Garbox0 <gbrlescalada@gmail.com>
2026-01-31 08:12:26 -0300

    chore: remove deleted verification scripts from repo

--- add_subscription_expiry.sql
+++
@@ -1,6 +0,0 @@
--- Añadir columna de fecha de expiración de la suscripción
-ALTER TABLE profiles 
-ADD COLUMN IF NOT EXISTS subscription_expiry TIMESTAMPTZ;
-
--- Comentario para documentación
-COMMENT ON COLUMN profiles.subscription_expiry IS 'Fecha en la que la suscripción profesional expira y requiere renovación.';
